0d3412fcf2c25092aefa776402d0fe9d2f80830d
Raul <raul@DESKTOP-6U1QTKD>
2017-04-14 15:42:38 +0200

    Creada la conexion con la base de datos. Ahora crear el formulario con el metodo POST

--- index.php
+++ index.php
@@ -1,9 +1,54 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
- 
- 
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 
- <?php
+
+<html>
+
+    <head>
+
+        <title>Autenticación DNIe Servidor</title>
+
+    </head>
+
+    <body>
+
+        <div class="texto">
+
+            <p><h2>Práctica 3. Implementación de un servicio básico de autenticación con DNIe</h2></p>
+
+    </div>
+
+    <div class="principal">
+
+        <div class="formulario">
+
+            <p><h3>Lista de usuarios</h3></p>
+
+            <ul>
+                <?php
+                    $link = mysql_connect('localhost:3306', 'root', '') or die('Error al conectar con el servidor');
+                    if (!$link) {
+                        die('Could not connect to MySQL: ' . mysql_error());
+                    }
+
+                    mysql_select_db('dni_db', $link) or die('Error al conectar con la base de datos');
+
+                    $sql = "SELECT * FROM usuarios";
+                    $resultado = mysql_query($sql);
+                    while ($row = mysql_fetch_assoc($resultado)) {
+
+                        echo "<li>" . $row["usuario"] . " " . $row["dni"] . "</li>";
+                    }
+                ?>
+            </ul>
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
